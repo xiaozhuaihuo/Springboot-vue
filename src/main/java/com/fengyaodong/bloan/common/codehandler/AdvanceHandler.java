757edfb88d0c479943b9c0a46ff7b0466053538f
feng_yd <feng_yd@suixingpay.com>
2019-07-05 14:42:08 +0800

    测试上传

---
+++ src/main/java/com/fengyaodong/bloan/common/codehandler/AdvanceHandler.java
@@ -0,0 +1,27 @@
+package com.fengyaodong.bloan.common.codehandler;
+
+/**
+ * 提前结清Handler
+ * 0：否 1：是
+ *
+ * @author: feng_yd[feng_yd@suixingpay.com]
+ * @date: 2019/5/5 11:35
+ * @version: V1.0
+ * @review: feng_yd[feng_yd@suixingpay.com]/2019/5/5 11:35
+ */
+public interface AdvanceHandler {
+
+    /**
+     * 获取提前结清Code
+     *
+     * @return
+     */
+    String getAdvanceHandlerCode();
+
+    /**
+     * 设置提前结清Text
+     *
+     * @param text
+     */
+    void setAdvanceHandlerText(String text);
+}
